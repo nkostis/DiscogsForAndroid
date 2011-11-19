1e39f6b28f8cf7bf7338dad48b8d9333f0b6e0ec
Andreas Daskalopoulos <mcaibad2@gmail.com>
2011-11-19 20:05:32 +0200

    initial commit with eclipse project

---
+++ DiscogsForAndroid/src/com/discogs/DiscogsForAndroidActivity.java
@@ -0,0 +1,14 @@
+package com.discogs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DiscogsForAndroidActivity extends Activity 
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
